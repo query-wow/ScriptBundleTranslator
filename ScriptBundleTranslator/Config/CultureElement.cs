aefdcca29c7c0527e0bdbc4584600e72b5ebbe25
Query <telmo.p.rodrigues@gmail.com>
2019-11-25 19:48:35 +0000

    Refactor

---
+++ ScriptBundleTranslator/Config/CultureElement.cs
@@ -0,0 +1,16 @@
+﻿using System.Configuration;
+
+namespace ScriptBundleTranslator.Config
+{
+    public class CultureElement : ConfigurationElement
+    {
+        [ConfigurationProperty("Name")]
+        public string Name => (string)base["Name"];
+
+        [ConfigurationProperty("Key", IsKey = true, IsRequired = true)]
+        public string Key => (string)base["Key"];
+
+        [ConfigurationProperty("TwoLetterIso", IsRequired = true)]
+        public string TwoLetterIso => (string)base["TwoLetterIso"];
+    }
+}
--- ScriptBundleTranslator/Config/CultureSettings.cs
+++ ScriptBundleTranslator/Config/CultureSettings.cs
@@ -29,35 +29,4 @@ namespace ScriptBundleTranslator.Config
             }
         }
     }
-
-    [ConfigurationCollection(typeof(CultureElement), AddItemName = "add")]
-    public class CultureElementCollection : ConfigurationElementCollection
-    {
-        protected override ConfigurationElement CreateNewElement()
-        {
-            return new CultureElement();
-        }
-
-        protected override object GetElementKey(ConfigurationElement element)
-        {
-            if (element == null)
-            {
-                throw new ArgumentNullException("element");
-            }
-
-            return ((CultureElement)element).Key;
-        }
-    }
-
-    public class CultureElement : ConfigurationElement
-    {
-        [ConfigurationProperty("Name")]
-        public string Name => (string)base["Name"];
-
-        [ConfigurationProperty("Key", IsKey = true, IsRequired = true)]
-        public string Key => (string)base["Key"];
-
-        [ConfigurationProperty("TwoLetterIso", IsRequired = true)]
-        public string TwoLetterIso => (string)base["TwoLetterIso"];
-    }
 }
